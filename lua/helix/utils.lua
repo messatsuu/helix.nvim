3330b601693c86afe3266858f65983c7e046cb25
messatsuu <hirsignicolas@gmail.com>
2024-09-29 20:00:14 +0200

    re-set spider motions when un-setting helix motions

--- lua/helix/utils.lua
+++ lua/helix/utils.lua
@@ -24,6 +24,12 @@ function M.disable_helix_mappings()
         vim.keymap.del(mode, 'k')
     end
 
+    -- re-set spider motions
+    vim.keymap.set({ "n", "o", "x" }, "w", function() require("spider").motion("w") end, { desc = "Spider-w" })
+    vim.keymap.set({ "n", "o", "x" }, "e", function() require("spider").motion("e") end, { desc = "Spider-e" })
+    vim.keymap.set({ "n", "o", "x" }, "b", function() require("spider").motion("b") end, { desc = "Spider-b" })
+    vim.keymap.set({ "n", "o", "x" }, "ge", function() require("spider").motion("ge") end, { desc = "Spider-ge" })
+
     mappings_active = false
 end
 
